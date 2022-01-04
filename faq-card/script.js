72f49bac4172fe1b2f469ec25ee1328d3c83dfa1
becmorrell <77584099+becmorrell@users.noreply.github.com>
2022-01-04 14:25:16 +0000

    4.1.22

--- faq-card/script.js
+++ faq-card/script.js
@@ -8,22 +8,15 @@ document.querySelectorAll(".question").forEach(item => {
         const question = e.target.closest(".question");
         console.log(question);
 
-        const answer = question.querySelector(".answer");
-        console.log(answer);
-
-        if(answer.classList.contains("expanded")) {
-            answer.classList.remove("expanded");
-
         if (question.classList.contains('expanded')) {
             question.classList.remove('expanded');
         }
-        }
+        
         else {
             const visibleElements = document.querySelectorAll(".expanded");
             visibleElements.forEach(item => {
                 item.classList.remove('expanded');
             });
-            answer.classList.add('expanded')
         question.classList.add('expanded')   }
     
     })
