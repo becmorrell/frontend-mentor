33d383014793c0afeeb63488a9a9eb512dbc297d
becmorrell <77584099+becmorrell@users.noreply.github.com>
2022-01-09 10:19:40 +0000

    meditation app

---
+++ meditation-app/script.js
@@ -0,0 +1,85 @@
+ const app = () => {
+   const song = document.querySelector(".song");
+   const play = document.querySelector(".play");
+   const outline = document.querySelector(".moving-outline circle");
+   const video = document.querySelector(".vid-container video");
+
+   //sounds
+   const sounds = document.querySelectorAll(".sound-picker button");
+
+   //time display
+   const timeDisplay = document.querySelector(".time-display");
+   const timeSelect = document.querySelectorAll(".time-select button");
+
+   //get the length of the outline
+   const outlineLength = outline.getTotalLength();
+   console.log(outlineLength);
+
+   //duration
+   let fakeDuration = 600;
+
+   outline.style.strokeDasharray = outlineLength;
+   outline.style.strokeDashoffset = outlineLength;
+
+   //play sounds
+   play.addEventListener('click', () => {
+     checkPlaying(song);
+   });
+
+   // get duration
+   timeSelect.forEach(option => {
+    option.addEventListener('click', function(){
+      fakeDuration = this.getAttribute('data-time');
+      timeDisplay.textContent = `${Math.floor(fakeDuration/60)}:${Math.floor(fakeDuration%60)}`;
+    });
+   });
+
+   //choose sound
+   sounds.forEach(sound => {
+     sound.addEventListener('click', function(){
+       song.src = this.getAttribute('data-sound');
+       video.src = this.getAttribute('data-video');
+       checkPlaying(song);
+     })
+
+   });
+
+
+   //play/pause button function
+   const checkPlaying = song => {
+     if(song.paused) {
+       song.play();
+       video.play()
+       play.src =`./svg/pause.svg`;
+     }
+     else {
+       song.pause();
+       video.play();
+       play.src =`./svg/play.svg`;
+     }
+   };
+
+   //Animate the Stroke
+   song.ontimeupdate = () => {
+     let currentTime = song.currentTime;
+     let elapsedTime = fakeDuration - currentTime;
+     let seconds = Math.floor(elapsedTime % 60);
+     let minutes = Math.floor(elapsedTime / 60);
+
+   //progress bar
+   let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+   outline.style.strokeDashoffset = progress;
+
+   //animate text
+   timeDisplay.textContent = `${minutes}:${seconds}`;
+
+   if(currentTime >= fakeDuration){
+     song.pause();
+     song.currentTime = 0;
+     play.src = './svg/play.svg';
+     video.pause();
+   }
+ };
+ }
+
+ app();
