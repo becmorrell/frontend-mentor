8cb39554f9eca0edd48cc5e6e452a9b06daf912d
becmorrell <77584099+becmorrell@users.noreply.github.com>
2022-01-13 10:23:43 +0000

    js update

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -2,23 +2,83 @@ const billAmount = document.querySelector("#bill-amount");
 const tipButtons = document.querySelectorAll(".btn");
 const customPercent = document.querySelector("#tip-percentage-input")
 const NOP = document.querySelector("#no-of-people");
+const elPeoplecontainer = document.querySelector('.NOP-container');
+const error = document.querySelector('.error-text');
 
 const reset = document.querySelector(".btn-reset");
 
+let tipOutput = document.querySelector(".tip-output");
+let totalOutput = document.querySelector(".total-output");
+
+let billValue = 0;
+let people = 0;
+let tipTotal = 0;
+let percentage = 0;
+let totalAmountpp = 0;
+
+//get value for user input of bill
+billAmount.addEventListener('input', function(){
+    billValue = parseFloat(billAmount.value);
+    console.log(billValue);
+    calculation();
+});
+
+//get value of tip button used
 tipButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-        console.log(e.target.value);
+      percentage = (e.target.value) / 100;
+        console.log(percentage);
+        calculation();
     })
+});
+
+//get custom value tip
+customPercent.addEventListener('input', function() {
+    percentage = (parseFloat(customPercent.value)) / 100; 
+    console.log(percentage);
+    calculation();
 })
 
-NOP.addEventListener("click", function() {
-    console.log(e.target)
+//get NOP entered
+NOP.addEventListener("input", function() {
+    people = parseInt(NOP.value);
+    console.log(people);
+    calculation();
 });
 
-billAmount.addEventListener("click", () => {
-    console.log(billAmount.value)
-});
+//reset button
+    reset.addEventListener("click", function() {
+        billAmount.value = null;
+        NOP.value = null;
+        customPercent.value = null;
 
-function updateValue(e) {
-    log.textContent = e.target.value;
-}
+    // reset variables
+    billValue = 0;
+    percentage = 0;
+    people = 0;    
+        
+    calculation();
+    })
+
+//tip amount = (billamount * 10%) / NOP
+//Total amount PP = ((Billamount 10%) + bill amount) / NOP
+
+function calculation() {
+    if (people > 0){
+        tipTotal = (billValue * percentage) / people;
+    tipOutput.textContent = tipTotal;
+
+    totalAmountpp = (billValue + tipTotal) / people;
+    totalOutput.textContent = totalAmountpp;  
+    
+    }
+    else if (people === 0)
+     {  elPeoplecontainer.classList.add(".error");
+        error.classList.add('.reveal-error');
+    }
+    else {
+        tipOutput.textContent = 0.00;
+        totalOutput.textContent  = 0.00;
+    }
+    } 
+    
